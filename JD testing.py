ea7db0e38424c73af0fc4b2e8a66655f54eaaf96
ApracsJ <scarpajd@gmail.com>
2026-04-03 22:38:24 -0400

    Angles are being properly determined and added to the figure display.

--- JD testing.py
+++ JD testing.py
@@ -20,40 +20,43 @@ a = 0
 # qc.x(a)
 # qc.id(a)
 # qc.cx(a)
+# qc.y(a)
 qc.h(a)
-qc.y(a)
+qc.s(a)
+qc.t(a)
 # qc.z(a)
-# qc.rz(np.pi / 2, a)
-
 
 # measure
 # qc.measure_all()
 
 # measurment = qc.cregs[0]
-
 # print(f"Register Name: {measurment.name}")
 # print(f"Number of bits: {measurment.size}")
 # print(f"Number of bits: {type(measurment)}")
+
+
 state = Statevector.from_instruction(qc)
 
 print(state.data)
-# Determine the angle(theta) from the 1st term
-phase = np.angle(state.data[0])
-temp = state.data * np.exp(-1j * phase)
-# print(f"{temp}")
-theta = np.arccos(np.real(temp[0])) * 2
 
+# Determine global phase of state vector from first term
+phase = np.angle(state.data[0])
+# Remove global phase from state vector (make 1st term only real)
+no_phase = state.data * np.exp(-1j * phase)
+# Determine theta from manitude of first term
+theta = np.arccos(np.real(no_phase[0])) * 2
 # Determine the phase(phi) of 2nd term
-phi = np.angle(temp[1])
-print(f"theta = {np.rad2deg(theta)}")
-print(f"phi = {phi}")
+phi = np.angle(no_phase[1])
+
+print(f"theta = {np.degrees(theta):.2f}")
+print(f"phi = {np.degrees(phi):.2f}")
 
 
 
 # ***** This Works ******
-# latex =state.draw(output='latex_source')
-# fig = plt.figure(figsize=(5, 2))
-# fig.text(0.5, 0.5, f"${latex}$", usetex=False)
+latex =state.draw(output='latex_source')
+fig = plt.figure(figsize=(5, 2))
+fig.text(0.5, 0.5, f"${latex}$", usetex=False)
+fig.text(0.5, 0.4, f"$\\theta={np.degrees(theta):.2f}\degree$, $\phi={np.degrees(phi):.2f}\degree$", usetex=False)
 
-# qc.draw("mpl")
-# plt.show() 
+plt.show() 
