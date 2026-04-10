aa6a52b34aa456062cfa401ab91dae456167b96f
ApracsJ <scarpajd@gmail.com>
2026-04-10 19:18:06 -0400

    Cleaned up comments and code

--- JD testing.py
+++ JD testing.py
@@ -1,62 +1,48 @@
 import matplotlib.pyplot as plt
 from qiskit import *
-from qiskit.circuit.library import HGate
 from qiskit.quantum_info import Statevector
-from IPython.display import display
-from IPython.display import Latex
-from qiskit.visualization import state_visualization
 import numpy as np
 
-# qc = QuantumCircuit(2)
-# qc.append(HGate(), [0])
-# qc.append(HGate(), [1])
-# qc.draw("mpl")
-# plt.show()
+# *** Quantum Circuit ***
+# Initialize a new quantum circuit object with one qubit
+qc = QuantumCircuit(1)
 
-qc = QuantumCircuit(2)
-
+# Apply quantum gates
 a = 0
-# qc.x(a)
-# qc.x(a)
-# qc.id(a)
-# qc.cx(a)
-# qc.y(a)
-qc.h(a)
-qc.s(a)
-qc.t(a)
-# qc.z(a)
-
-# measure
-# qc.measure_all()
-
-# measurment = qc.cregs[0]
-# print(f"Register Name: {measurment.name}")
-# print(f"Number of bits: {measurment.size}")
-# print(f"Number of bits: {type(measurment)}")
+qc.h(a)     # Hadamard Gate -> creates a superposition state
+qc.s(a)     # Clifford gate S Gate -> adds a phase of pi/2
+qc.t(a)     # T Gate -> adds a phase shift of 1/4 pi to the 1 state
 
 
+# *** Determine State Vector ***
+# Obtain statevector object from quantum circuit
 state = Statevector.from_instruction(qc)
-
-print(state.data)
 
 # Determine global phase of state vector from first term
 phase = np.angle(state.data[0])
 # Remove global phase from state vector (make 1st term only real)
 no_phase = state.data * np.exp(-1j * phase)
-# Determine theta from manitude of first term
+
+# Determine theta from magnitude of first term
 theta = np.arccos(np.real(no_phase[0])) * 2
-# Determine the phase(phi) of 2nd term
+
+# Determine the phase (phi) of 2nd term
 phi = np.angle(no_phase[1])
 
+# Display state vector to terminal
+print(state.data)
 print(f"theta = {np.degrees(theta):.2f}")
 print(f"phi = {np.degrees(phi):.2f}")
 
 
+# *** Visual Display ***
+# Obtain a latex IPython Latex object from statevector
+latex = state.draw(output = 'latex_source')
+# Create a new matplotlib figure
+fig = plt.figure(figsize = (6, 2.5))
+# Add text to figure to display the resulting quantum 
+# state vector as well as its corresponding spherical cordinates
+fig.text(0.5, 0.5, f"${latex}$", ha='center', usetex = False)
+fig.text(0.5, 0.4, fr"$\theta={np.degrees(theta):.2f}\degree$, $\phi={np.degrees(phi):.2f}\degree$", ha='center', usetex = False)
 
-# ***** This Works ******
-latex =state.draw(output='latex_source')
-fig = plt.figure(figsize=(5, 2))
-fig.text(0.5, 0.5, f"${latex}$", usetex=False)
-fig.text(0.5, 0.4, f"$\\theta={np.degrees(theta):.2f}\degree$, $\phi={np.degrees(phi):.2f}\degree$", usetex=False)
-
-plt.show() 
+plt.show()
